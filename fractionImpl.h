395c4aecc9fd39aab025fba3cfea7010c400fded
pozniako16 <lev.pozniakoff@gmail.com>
2020-03-30 13:44:59 +0200

    ajout des fonction dans Impl.h

--- fractionImpl.h
+++ fractionImpl.h
@@ -14,4 +14,53 @@
 #ifndef FRACTIONIMPL_H
 #define FRACTIONIMPL_H
 
+//
+// Created by Lev on 30.03.2020.
+//
+#include "fraction.h"
+#include <iostream>
+
+#define type T
+#define tmplt template<typename type>
+
+#ifndef FRACTIOIMPLN_H
+#define FRACTIONIMPL_H
+
+using namespace std;
+
+tmplt
+ostream& operator<<(ostream& os, const Fraction<T>& rhs){
+
+}
+
+tmplt
+Fraction<T>::Fraction<T>(type n, type d) : numerateur(n), denominateur(d) {}
+
+tmplt
+Fraction<T>::operator float() const {}
+
+tmplt
+Fraction<T>::operator double() const{}
+
+tmplt
+Fraction<T> Fraction<T>::simplifier() const {}
+
+tmplt
+bool identite(const Fraction<T>& f) const {}
+
+tmplt
+bool Fraction<T>::operator== (const Fraction<T>& rhs) const {}
+
+tmplt
+Fraction<T> Fraction<T>::operator+ (const Fraction<T>& rhs) const{}
+
+tmplt
+Fraction<T>& Fraction<T>::operator += (const Fraction<T>& rhs){}
+
+tmplt
+Fraction<T> Fraction<T>::operator* (const Fraction<T>& rhs) const{}
+
+tmplt
+Fraction<T>& Fraction<T>::operator *= (const Fraction<T>& rhs){};
+
 #endif
